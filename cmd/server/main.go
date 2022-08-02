d80edef036490f8131ccd06d92398f4f82538394
Dominic Faulring <dfaulring@gmail.com>
2022-08-02 07:20:57 -0700

    structuring our application

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,22 @@
 package main
 
-import(
+import (
 	"fmt"
 )
 
+type App struct {
+}
+
+func (app *App) Run() error {
+	fmt.Println("Setting up our app")
+	return nil
+}
+
 func main() {
 	fmt.Println("GO REST API")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up our REST API")
+		fmt.Println(err)
+	}
 }
